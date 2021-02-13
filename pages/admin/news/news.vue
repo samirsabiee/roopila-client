c12d2f0ff63e52c44460c64a2e4360c7237eb997
Samir Sabiee <sabieesamir@gmail.com>
2021-02-13 15:49:04 +0330

    edit news page

--- pages/admin/news/news.vue
+++ pages/admin/news/news.vue
@@ -1,20 +1,24 @@
 <template>
   <b-row class="mt-5" align-h="center">
     <b-col cols="8" class="bg-light p-2">
-      <div class="news-box overflow-hidden rounded">
-        <img class="img-cover-center img-fluid h-100" src="" alt="news">
-        <div class="darkLayer d-flex flex-column justify-content-end align-items-start w-100 p-2">
-          <div class="d-flex flex-row justify-content-between align-items-center w-100">
-            <h6 class="text-white border border-success p-1 category-font-size">اقتصادی</h6>
-            <i class="far fa-eye" style="font-size: 1.5rem; color: greenyellow;"> 357</i>
+      <b-row>
+        <b-col cols="4">
+          <div  class="news-box overflow-hidden rounded border border-success">
+            <img class="img-cover-center img-fluid h-100" src="" alt="news">
+            <div class="darkLayer d-flex flex-column justify-content-end align-items-start w-100 p-2">
+              <div class="d-flex flex-row justify-content-between align-items-center w-100">
+                <h6 class="text-white border border-success p-1 category-font-size">اقتصادی</h6>
+                <i class="far fa-eye" style="font-size: 1.5rem; color: greenyellow;"> 357</i>
+              </div>
+              <h5>عنوان خبر</h5>
+              <h6 class="date-font-size">تاریخ 99/11/02</h6>
+            </div>
           </div>
-          <h5>عنوان خبر</h5>
-          <h6 class="date-font-size">تاریخ 99/11/02</h6>
-        </div>
-      </div>
+        </b-col>
+      </b-row>
     </b-col>
     <b-col cols="3" class="bg-light mr-1 p-2">
-      <h4>دسته بندی ها</h4>
+      <h4 class="text-center">دسته بندی ها</h4>
       <hr class="w-100">
       <h5 class="btn btn-outline-success btn-block">واردات</h5>
       <h5 class="btn btn-outline-success btn-block">صادرات</h5>
--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -1,26 +1,30 @@
 <template>
   <b-container fluid>
-    <input class="form-control m-3" type="text" placeholder="جستجوی کاربر">
-    <table class="table table-striped table-bordered text-center m-3">
-      <thead>
-      <tr>
-        <th>#</th>
-        <th>نام و نام خانوادگی</th>
-        <th>کد ملی</th>
-        <th>تعداد سفارشات</th>
-        <th>عملیات</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>1</td>
-        <td>سمیر سبیعی</td>
-        <td>3490299221</td>
-        <td>20</td>
-        <td><i class="fas fa-eye"></i></td>
-      </tr>
-      </tbody>
-    </table>
+    <b-row>
+      <b-col>
+        <b-form-input class="m-3" placeholder="جستجوی کاربر"></b-form-input>
+        <table class="table table-striped table-bordered text-center m-3">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>نام و نام خانوادگی</th>
+            <th>کد ملی</th>
+            <th>تعداد سفارشات</th>
+            <th>عملیات</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>1</td>
+            <td>سمیر سبیعی</td>
+            <td>3490299221</td>
+            <td>20</td>
+            <td><i class="fas fa-eye"></i></td>
+          </tr>
+          </tbody>
+        </table>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
