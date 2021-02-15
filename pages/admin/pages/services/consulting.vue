4bf38130066563c00b0b5769e4d1590395552318
Samir Sabiee <sabieesamir@gmail.com>
2021-02-15 16:03:32 +0330

    add pages to cms

--- pages/admin/pages/services/consulting.vue
+++ pages/admin/pages/services/consulting.vue
@@ -6,16 +6,22 @@
         <div>
           <img class="main_feature" src="static/img/icons/feature.png" alt="main feature">
         </div>
-        <p class="pr-2 pl-2 justify-text pt-4rem"> متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی
-          اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی</p>
+        <p v-b-modal.modal-feature-content v-text="pageContent.featureContent" class="pr-2 pl-2 justify-text pt-4rem"></p>
+        <b-modal hide-footer size="lg" id="modal-feature-content">
+          <b-form-textarea v-model="pageContent.featureContent"></b-form-textarea>
+        </b-modal>
       </div>
     </div>
-
     <div class="d-flex flex-row justify-content-start align-items-start jumbotron">
       <div class="col-6">
-        <h1 class="text-center">عنوان مشاوره</h1>
-        <p class="p-3 justify-text">متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن
-          مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره </p>
+        <h1 v-b-modal.modal-title v-text="pageContent.title" class="text-center">عنوان مشاوره</h1>
+        <b-modal hide-footer id="modal-title">
+            <b-form-input v-model="pageContent.title"></b-form-input>
+        </b-modal>
+        <p v-b-modal.modal-mainContent v-text="pageContent.mainContent" class="p-3 justify-text"></p>
+        <b-modal hide-footer size="xl" id="modal-mainContent">
+            <b-form-textarea v-model="pageContent.mainContent"></b-form-textarea>
+        </b-modal>
       </div>
     </div>
   </div>
@@ -24,7 +30,19 @@
 <script>
   export default {
     name: "consulting",
-    layout: "admin"
+    layout: "admin",
+    data() {
+      return {
+        pageContent: {
+          image:'',
+          title:'عنوان مشاوره',
+          mainContent:'متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن' +
+            '          مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره متن مشاوره ',
+          featureContent:' متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی' +
+            '          اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی متن ویژگی اصلی'
+        }
+      }
+    },
   }
 </script>
 
--- pages/transport.vue
+++ pages/transport.vue
@@ -4,7 +4,7 @@
         <img class="img-cover-center transport-image w-100 img-fluid" src="static/img/transport.jpg" alt="transport">
         <div class="darkLayer w-100 d-flex flex-row justify-content-end align-items-center">
 
-          <div class="col-6 d-flex flex-column jusify-content-center align-items-center">
+          <div class="col-6 d-flex flex-column justify-content-center align-items-center">
             <h1 class="text-white">حمل و نقل</h1>
             <h3 class="text-white"> ارسال محموله از مریخ به تمامی نقاط کشور</h3>
             <button class="btn btn-outline-success">ارسال درخواست</button>
@@ -16,18 +16,18 @@
       <div class="d-flex flex-row justify-content-center align-items-center">
         <div class="col-2 transport-type-box bg-light shadow shadow-sm rounded overflow-hidden p-0">
           <img class="img-cover-center w-100 transport-box-img img-fluid" src="static/img/small-truck.jpg" alt="earch transport">
-          <h5 class="font-weight-bold pt-3 pr-3">حمل نقل زمینی</h5>
-          <p class="pr-3 pl-3">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
+          <h5 class="font-weight-bold pt-3 pr-3 text-center">حمل نقل زمینی</h5>
+          <p class="pr-3 pl-3 text-center">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
         </div>
         <div class="col-2 transport-type-box bg-light shadow shadow-sm rounded overflow-hidden p-0">
           <img class="img-cover-center w-100 transport-box-img img-fluid" src="static/img/small-ship.jpg" alt="earch transport">
-          <h5 class="font-weight-bold pt-3 pr-3">حمل نقل دریایی</h5>
-          <p class="pr-3 pl-3">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
+          <h5 class="font-weight-bold pt-3 pr-3 text-center">حمل نقل دریایی</h5>
+          <p class="pr-3 pl-3 text-center">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
         </div>
         <div class="col-2 transport-type-box bg-light shadow shadow-sm  rounded overflow-hidden p-0">
           <img class="img-cover-center w-100 transport-box-img img-fluid" src="static/img/small-airplane.jpg" alt="earch transport">
-          <h5 class="font-weight-bold pt-3 pr-3">حمل نقل هوایی</h5>
-          <p class="pr-3 pl-3">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
+          <h5 class="font-weight-bold pt-3 pr-3 text-center">حمل نقل هوایی</h5>
+          <p class="pr-3 pl-3 text-center">متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم متن لورام اپیزوم </p>
         </div>
       </div>
 
