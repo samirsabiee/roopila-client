aca5008383781da14d7eae79b83791abbbf2a622
Samir Sabiee <sabieesamir@gmail.com>
2021-03-14 08:58:04 +0330

    set background gallery backgroud label

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -5,7 +5,7 @@
       <div class="row" >
         <ul class="col container-filter list-unstyled categories-filter text-center" id="filter">
           <li v-for="(category,index) in categoryItems" :key="index" @click="setCategoryIndex(index)" class="list-inline-item"><a
-            :class="`categories border d-block text-dark rounded ${(index === 0)? 'active':''}`">{{category.name}}</a>
+            :class="`categories border d-block text-dark rounded ${(index === categoryIndex)? 'active':''}`">{{category.name}}</a>
           </li>
         </ul>
       </div><!--end row-->
@@ -14,7 +14,7 @@
     <div class="container-fluid">
       <div class="row container-grid projects-wrapper">
 
-        <div v-for="(image,index) in categoryItems[categoryIndex].images" class="col-lg-3 col-md-6 spacing designing">
+        <div v-for="(image,index) in categoryItems[categoryIndex].images" class="col-lg-3 col-md-6 spacing designing fadeIn-animation">
           <div class="work-container position-relative d-block overflow-hidden rounded mt-3">
             <a class="mfp-image d-inline-block" title="">
               <img :src="image.image" class="img-fluid rounded" alt="work-image">
@@ -50,12 +50,14 @@
     },
     methods: {
       async galleryCategories() {
+        this.startLoading()
         let categories = await this.$apollo.query({
           query: galleryCategories,
           variables: {limit: 8}
         })
         this.categoryItems = categories.data.galleryCategories.galleryCategories
         this.loading = false
+        this.finishLoading()
       },
       setCategoryIndex(index){
         this.categoryIndex = index
